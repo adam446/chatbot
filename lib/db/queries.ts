32785fe4df6776fbcd27fbea260b83902f661dde
adam446 <adam.bouatayw@gmail.com>
2026-07-17 15:50:43 -0400

    fix postgresql ssl and chat stream

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -33,7 +33,7 @@ import {
 } from "./schema";
 import { generateHashedPassword } from "./utils";
 
-const client = postgres(process.env.POSTGRES_URL ?? "");
+const client = postgres(process.env.POSTGRES_URL ?? "", { ssl: "require" });
 const db = drizzle(client);
 
 export async function getUser(email: string): Promise<User[]> {
